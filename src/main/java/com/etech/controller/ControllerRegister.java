8544e7c03a91ee9069e1c23d5042e1bd22c9d33b
wuqiwei <1058633117@qq.com>
2014-05-26 17:29:41 +0800

    修改ControllerRegister.java

--- src/main/java/com/etech/controller/ControllerRegister.java
+++ src/main/java/com/etech/controller/ControllerRegister.java
@@ -25,13 +25,28 @@ public class ControllerRegister {
 	private static final Log log = LogFactory.getLog(ControllerRegister.class);
 	@Resource
 	private CommonUserService commonUserService;
-	/** Begin Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:企业用户注册*/
+	/** Begin Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:企业用户注册页面*/
 	@RequestMapping(value = "/personalReg")
-	public String personalRegister(TCommonUser user,HttpServletRequest request) {
-		String password = DigestUtils.md5Hex(user.getPassword());
-		user.setCreateDate(System.currentTimeMillis());
-		user.setPassword(password);
+	public String personalRegister() {
 		return "register/personalRegister";
+	}
+	/** End Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:普通用户注册页面*/
+	
+	/** Begin Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:普通用户注册*/
+	public String submitpersonalReg(TCommonUser user,HttpServletRequest request){
+		if(user.getLoginName()!=null || user.getPassword()!=null){
+			String referer=request.getHeader("Referer");
+			String password = DigestUtils.md5Hex(user.getPassword());
+			user.setCreateDate(System.currentTimeMillis());
+			user.setEditDate(System.currentTimeMillis());
+			user.setPassword(password);
+			commonUserService.saveOrUpdate(user);
+			return "redirect:"+referer; 
+		}else{
+			request.setAttribute("email", user.getEmail());
+			request.setAttribute("phoneNum", user.getPhoneNum());
+			return "register/personalRegister";
+		}
 	}
 	/** End Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:普通用户注册*/
 	
@@ -42,15 +57,13 @@ public class ControllerRegister {
 		TCommonUser user = (TCommonUser)commonUserService.findObjectByProperty(TCommonUser.class, EtechGobal.LoginName, loginName);
 		Map<String, Object> message=new HashMap<String, Object>();
 		if (user!=null) {
-			message.put("error","error");
-			message.put("message","用户名已存在，请重新填写用户名");
-			JsonOutToBrower.out(message, response);
-			return;
-		}else {
-			message.put("success","success");
-			JsonOutToBrower.out(message, response);
+			message.put("messageLoginName","用户名已存在，请重新填写用户名");
 		}
-		TCommonUser user = (TCommonUser)commonUserService.findObjectByProperty(TCommonUser.class, EtechGobal.Idcard, idcard);
+		user = (TCommonUser)commonUserService.findObjectByProperty(TCommonUser.class, EtechGobal.Idcard, idcard);
+		if (user!=null) {
+			message.put("messagePwd","身份证号已存在,请重新填写");
+		}
+		JsonOutToBrower.out(message, response);
 	}
 	/** End Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:ajax判断用户名是否已经存在*/
 	
