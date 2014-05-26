32adfef366ab8964712c15c6df6705e2cb5e8318
wuqiwei <1058633117@qq.com>
2014-05-26 17:48:21 +0800

    add table for EnterpriseUser.java

--- src/main/java/com/etech/entity/EnterpriseUser.java
+++ src/main/java/com/etech/entity/EnterpriseUser.java
@@ -2,15 +2,19 @@ package com.etech.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /** 企业用户 */
+@Entity
+@Table(name="TEnterpriseUser")
 public class EnterpriseUser implements Serializable {
 	
 	private static final long serialVersionUID = 6293704345783832975L;
-	public static final String LoginName="loginName";
 	private int id;
 	/** 用户名 */
 	private String loginName;
@@ -40,7 +44,7 @@ public class EnterpriseUser implements Serializable {
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	@Column(nullable = false,columnDefinition="varchar(30) comment '登录名' default '' ")
 	public String getLoginName() {
 		return loginName;
 	}
@@ -48,11 +52,7 @@ public class EnterpriseUser implements Serializable {
 	public void setLoginName(String loginName) {
 		this.loginName = loginName;
 	}
-
-	public static String getLoginname() {
-		return LoginName;
-	}
-
+	@Column(nullable = false,columnDefinition="varchar(40) comment '登录密码'  default '' ")
 	public String getPassword() {
 		return password;
 	}
@@ -60,7 +60,8 @@ public class EnterpriseUser implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	@Column(nullable = false,columnDefinition="varchar(30) comment '公司电话号码' default '' ")
 	public String getCpPhoneNum() {
 		return cpPhoneNum;
 	}
@@ -68,15 +69,18 @@ public class EnterpriseUser implements Serializable {
 	public void setCpPhoneNum(String cpPhoneNum) {
 		this.cpPhoneNum = cpPhoneNum;
 	}
-
+	
+	@Column(nullable = false,columnDefinition="varchar(30) comment '企业邮箱' default '' ")
 	public String getCpEmail() {
 		return cpEmail;
 	}
-
+	
+	
 	public void setCpEmail(String cpEmail) {
 		this.cpEmail = cpEmail;
 	}
-
+	
+	@Column(nullable = false,columnDefinition="varchar(30) comment '企业名' default '' ")
 	public String getCpName() {
 		return cpName;
 	}
@@ -84,7 +88,8 @@ public class EnterpriseUser implements Serializable {
 	public void setCpName(String cpName) {
 		this.cpName = cpName;
 	}
-
+	
+	@Column(nullable = false,columnDefinition="varchar(30) comment '企业地址' default '' ")
 	public String getCpAddres() {
 		return cpAddres;
 	}
@@ -92,7 +97,7 @@ public class EnterpriseUser implements Serializable {
 	public void setCpAddres(String cpAddres) {
 		this.cpAddres = cpAddres;
 	}
-
+	@Column(nullable = false,columnDefinition="varchar(30) comment '企业联系方式' default '' ")
 	public String getCpContacts() {
 		return cpContacts;
 	}
@@ -100,7 +105,8 @@ public class EnterpriseUser implements Serializable {
 	public void setCpContacts(String cpContacts) {
 		this.cpContacts = cpContacts;
 	}
-
+	
+	@Column(nullable = false,columnDefinition="varchar(30) comment '企业类型' default '' ")
 	public String getKindOfcp() {
 		return kindOfcp;
 	}
