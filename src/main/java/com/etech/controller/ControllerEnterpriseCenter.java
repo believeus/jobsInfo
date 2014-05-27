31215293d81afa5498288fc65462585f3a9f1175
wuqiwei <1058633117@qq.com>
2014-05-27 15:02:13 +0800

    ControllerEnterpriseCenter.java

---
+++ src/main/java/com/etech/controller/ControllerEnterpriseCenter.java
@@ -0,0 +1,21 @@
+package com.etech.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 个人中心
+ * */
+@Controller
+public class ControllerEnterpriseCenter {
+	private static Log log = LogFactory.getLog(ControllerEnterpriseCenter.class);
+
+	@RequestMapping(value = "/enterpriseCenter", method = RequestMethod.GET)
+	public String enterpriseCenter() {
+		log.debug("current controller is enterpriseCenter !");
+		return "center/enterpriseCenter";
+	}
+}
--- src/main/java/com/etech/controller/ControllerRegister.java
+++ src/main/java/com/etech/controller/ControllerRegister.java
@@ -4,14 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.etech.entity.EnterpriseUser;
@@ -37,15 +36,15 @@ public class ControllerRegister {
 	
 	/** Begin Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:ajax判断一般用户的ajax验证*/
 	@RequestMapping(value="/ajaxComValidReg")
-	public void ajaxComValidReg(TUser regUser,String submit,HttpSession session,HttpServletResponse response){
+	public void ajaxComValidReg(TCommonUser regUser,String submit,HttpSession session,HttpServletResponse response){
 		log.debug("current regUser reginName:"+regUser.getLoginName());
 		Map<String, Object> message=new HashMap<String, Object>();
-		if("".equals(regUser.getLoginName())){
+		if(StringUtils.isEmpty(regUser.getLoginName())){
 			message.put("message","用户名必填!");
 			JsonOutToBrower.out(message, response);
 			return;
 		}
-		if(!"".equals(regUser.getIdcard())){
+		if(!StringUtils.isEmpty(regUser.getIdcard())){
 			//身份证正则表达式
 			boolean matcheIdCard1 = regUser.getIdcard().matches("^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])((\\d{4})|\\d{3}[A-Z])$");
 			boolean matcheIdCard2 = regUser.getIdcard().matches("/^[1-9]\\d{7}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}$/");
@@ -57,13 +56,13 @@ public class ControllerRegister {
 			}
 		}
 		TUser user = (TUser) userService.findObjectByProperty(TCommonUser.class, EtechGobal.LoginName, regUser.getLoginName());
-		if (user!=null) {
+		if (!StringUtils.isEmpty(user)) {
 			message.put("message","用户名已存在，请重新填写用户名");
 			JsonOutToBrower.out(message, response);
 			return;
 		}
 		user = (TUser) userService.findObjectByProperty(TCommonUser.class, EtechGobal.Idcard, regUser.getIdcard());
-		if (user!=null) {
+		if (!StringUtils.isEmpty(user)) {
 			message.put("message","身份证号已存在,请重新填写");
 			JsonOutToBrower.out(message, response);
 			return;
@@ -87,11 +86,11 @@ public class ControllerRegister {
 	
 	/**Begin Author:wuqiwei Data:2014-05-26  Email:1058633117@qq.com:AddReason:企业注册登陆名验证*/
 	@RequestMapping(value="/ajaxEnterpriseValidReg")
-	public void ajaxEnterpriseValidReg(String loginName,HttpServletResponse response){
-		log.debug("loginName:"+loginName);
-		TUser user = (TUser) userService.findObjectByProperty(EnterpriseUser.class, EtechGobal.LoginName, loginName);
+	public void ajaxEnterpriseValidReg(EnterpriseUser regUser,HttpServletResponse response){
+		log.debug("loginName:"+regUser.getLoginName());
+		TUser user = (TUser) userService.findObjectByProperty(EnterpriseUser.class, EtechGobal.LoginName, regUser.getLoginName());
 		Map<String, Object> message=new HashMap<String, Object>();
-		if (user!=null) {
+		if (StringUtils.isEmpty(user)) {
 			message.put("messageLoginName","用户名已存在，请重新填写用户名");
 		}
 		JsonOutToBrower.out(message, response);
@@ -104,24 +103,5 @@ public class ControllerRegister {
 		return "register/enterpriseRegister";
 	}
 	/** End Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:企业用户注册*/
-	/** Begin Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:普通用户注册*/
-	@RequestMapping(value="/submitEnterpriseReg")
-	public String submitEnterpriseReg(EnterpriseUser user,HttpServletRequest request){
-		if(user.getLoginName()!=null || user.getPassword()!=null){
-			String referer=request.getHeader("Referer");
-			String password = DigestUtils.md5Hex(user.getPassword());
-			user.setPassword(password);
-			userService.saveOrUpdate(user);
-			return "redirect:"+referer; 
-		}else{
-			//在页面上保留用户已经填写过的信息
-			request.setAttribute("cpAddres", user.getAddress());
-			request.setAttribute("cpContacts", user.getCpContacts());
-			request.setAttribute("cpPhoneNum", user.getPhoneNum());
-			request.setAttribute("cpEmail", user.getEmail());
-			return "register/personalRegister";
-		}
-	}
-	/** End Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com AddReason:普通用户注册*/
 }
 /** End Author:wuqiwei Data:2014=05-26 Email:1058633117@qq.com CreateReason:企业用户和普通用户注册*/
