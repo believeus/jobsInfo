05ff57d00bf5f1ca45419ad9cc0742849414e2f8
wuqiwei <1058633117@qq.com>
2014-06-25 15:11:30 +0800

    CommonUserService.java

--- src/main/java/com/etech/service/CommonUserService.java
+++ src/main/java/com/etech/service/CommonUserService.java
@@ -22,56 +22,26 @@ import com.etech.entity.Trecruit;
 public class CommonUserService {
 	private static final Log log=LogFactory.getLog(CommonUserService.class);
 	@Resource
-	private EtechComDao etechComDao;
-
-	public EtechComDao getEtechComDao() {
-		return etechComDao;
-	}
-
-	public void setEtechComDao(EtechComDao etechComDao) {
-		this.etechComDao = etechComDao;
-	}
+	private EtechService etechService;
 	/*Begin Author:wuqiwei Data:2014-06-18 AddReason:根据填写的志愿信息获取推荐企业*/
 	@SuppressWarnings("unchecked")
 	public List<Trecruit> enterpriseRecommend(List<TcomInfo> comInfoList){
 		List<Trecruit> recruitList=new ArrayList<Trecruit>();
-		String hql=null;
-		List<Trecruit> recruits=new ArrayList<Trecruit>();
 		for (Iterator<TcomInfo> iterator = comInfoList.iterator(); iterator.hasNext();) {
 			TcomInfo tcomInfo = (TcomInfo) iterator.next();
-			if(!StringUtils.isEmpty(tcomInfo.getWorkspace())){
-				hql="From Trecruit recruit "
-				    + "where recruit.workType.id="+tcomInfo.getWorkType().getId()+" "
-				    + "and recruit.workspace='"+tcomInfo.getWorkspace()+"'";
-				log.debug(hql);
-				// 查询三个
-				recruits=(List<Trecruit>) etechComDao.getPageDateList(hql, 3);
-				if(!StringUtils.isEmpty(recruits)){
-					recruitList.addAll(recruits);
-				}
-			}
-			// 不够三个再查,直到填满三个为止
-			if(recruits.isEmpty() || recruits.size()<3){
-				hql="From Trecruit recruit where recruit.workType.id='"+tcomInfo.getWorkType().getId()+"'";
-				log.debug(hql);
-				recruitList.addAll((List<Trecruit>) etechComDao.getPageDateList(hql, 3-(recruits.size())));
+			if(!StringUtils.isEmpty(tcomInfo.getWorkType())){
+				String hql="From Trecruit recruit where recruit.workType.id='"+tcomInfo.getWorkType().getId()+"'";
+				List<Trecruit> recruits = (List<Trecruit>)etechService.findListByHQL(hql, 15);
+				recruitList.addAll(recruits);
 			}
 		}
-		if(!StringUtils.isEmpty(recruitList)){
-			//去重复
-			 Set<Trecruit> h = new HashSet<Trecruit>(recruitList);
-			 recruitList.clear();
-			 recruitList.addAll(h);
-			 // 随机取三个
-			 Collections.shuffle(recruitList);
-			 // 元素个数大于3
-			 if(recruitList.size()>3){
-				 recruitList.subList(0, 3);
-			// 元素个数小于3
-			 }else {
-				return recruitList;
+		if(!recruitList.isEmpty()){
+			Collections.shuffle(recruitList);
+			if (recruitList.size()>3) {
+				recruitList.subList(0, 3);
 			}
 		}
+		// 随机
 		return recruitList;
 	} 
 }
--- src/main/java/com/etech/service/EnterpriseUserService.java
+++ src/main/java/com/etech/service/EnterpriseUserService.java
@@ -2,13 +2,9 @@ package com.etech.service;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-
 import javax.annotation.Resource;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
@@ -23,44 +19,22 @@ public class EnterpriseUserService {
 	@Resource
 	private EtechService etechService;
 	// 企业人才推荐
+	@SuppressWarnings("unchecked")
 	public List<TcomUser> talentRecommend(List<Trecruit> recruitList){
 		List<TcomUser> userList=new ArrayList<TcomUser>();
-		List<TcomUser> users=new ArrayList<TcomUser>();
-		String hql="";
 		for (Iterator<Trecruit> iterator = recruitList.iterator(); iterator.hasNext();) {
 			Trecruit recruit = (Trecruit) iterator.next();
-			log.debug("workspace:"+recruit.getWorkspace()+" workType:"+recruit.getWorkType());
-			//匹配度最高的符合工作地区,又符合工作类型
-			if(!StringUtils.isEmpty(recruit.getWorkspace())&&!StringUtils.isEmpty(recruit.getWorkType())){
-				hql="from TcomUser user inner join fetch user.comInfo userInfo  where  userInfo.workType.id='"+recruit.getWorkType().getId()+"' "
-						+ "and userInfo.workspace='"+recruit.getWorkspace()+"'";
-				log.debug(hql);
-				users = (List<TcomUser>)etechService.findListByHQL(hql, 3);
-				if(!users.isEmpty()){
-					userList.addAll(users);
-				}
-				// 查找到匹配度最高的就不往下查找了
-				if(userList.size()>=3){
-					Collections.shuffle(userList);
-					continue;
-				}
+			if(!StringUtils.isEmpty(recruit.getWorkType())){
+				int workType=recruit.getWorkType().getId();
+				String hql="from TcomUser user inner join fetch user.comInfo userInfo  where  userInfo.workType.id='"+workType+"'";
+				userList.addAll((List<TcomUser>)etechService.findListByHQL(hql, 15));
 			}
-			//只匹配工作类型
-			if(userList.size()<3&&!StringUtils.isEmpty(recruit.getWorkType())){
-				userList.addAll(users);
-				hql="from TcomUser user inner join fetch user.comInfo userInfo  where  userInfo.workType.id='"+recruit.getWorkType().getId()+"'";
-				log.debug(hql);
-				userList.addAll((List<TcomUser>)etechService.findListByHQL(hql, (3-userList.size())));
-				//去重复
-				 Set h = new HashSet(userList);
-				 userList.clear();
-				 userList.addAll(h);
-				 Collections.shuffle(userList);
-				 if(userList.size()<3){
-					 return userList;
-				 }
-				 // 随机取三个
-				 userList.subList(0, 3);
+		}
+		if(!userList.isEmpty()){
+			Collections.shuffle(userList);
+			//如果个数大于三则截取三个
+			if (userList.size()>3) {
+				userList.subList(0, 3);
 			}
 		}
 		return userList;
