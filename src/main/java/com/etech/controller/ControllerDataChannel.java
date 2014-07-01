e90351c221f3e60ddad136c84dadbfba8de8429f
wuqiwei <1058633117@qq.com>
2014-07-01 14:17:57 +0800

    ControllerDataChannel.java

--- src/main/java/com/etech/controller/ControllerDataChannel.java
+++ src/main/java/com/etech/controller/ControllerDataChannel.java
@@ -3,10 +3,12 @@ package com.etech.controller;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,27 +31,27 @@ public class ControllerDataChannel {
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/dataChannel", method = RequestMethod.GET)
-	public String dataChanelView(HttpSession session) {
+	public String dataChanelView(HttpServletRequest request) {
 		
 		//公告公示
 		String hql="From TdataCenter dataCenter where dataCenter.type='2'";
 		List<TdataCenter> notices = (List<TdataCenter>)etechService.findListByHQL(hql);
-		session.setAttribute("notices",notices );
+		request.setAttribute("notices",notices );
 		
 		//专题
 		hql="From TdataCenter dataCenter where dataCenter.type='5'";
 		List<TdataCenter> subjectReport = (List<TdataCenter>)etechService.findListByHQL(hql);
-		session.setAttribute("subjectReport", subjectReport);
+		request.setAttribute("subjectReport", subjectReport);
 		
 		//供求分析报告
 		hql="From TdataCenter dataCenter where dataCenter.type='14'";
 		List<TdataCenter> analysisreport = (List<TdataCenter>)etechService.findListByHQL(hql);
-		session.setAttribute("analysisreport", analysisreport);
+		request.setAttribute("analysisreport", analysisreport);
 		
 		//工资指导价位
 		hql="From TdataCenter dataCenter where dataCenter.type='15'";
 		List<TdataCenter> wagePriceGuide = (List<TdataCenter>)etechService.findListByHQL(hql);
-		session.setAttribute("wagePriceGuide", wagePriceGuide);
+		request.setAttribute("wagePriceGuide", wagePriceGuide);
 		
 		// 获取一个月的毫秒数
 		long oneMonthTimeMillis=TimeUtils.getOneMonthTimeMillis();
@@ -62,20 +64,20 @@ public class ControllerDataChannel {
 		   + "group by recruit.workType "
 		   + "order by recruit.editTime desc";
 		log.debug(hql);
-		List<Trecruit> demand = (List<Trecruit>)etechService.findListByHQL(hql,8);
-		session.setAttribute("demand", demand);
+		List<Trecruit> demand = (List<Trecruit>)etechService.findListByHQL(hql,10);
+		request.setAttribute("demand", demand);
 		
 		//每月供给排行
 		hql="from TcomInfo info left join fetch info.comUser "
 		  + "where info.editDate>="+beginTime+" and info.editDate<="+endTime+" group by info.workType";
 		log.debug(hql);
-		List<TcomInfo> supply = (List<TcomInfo>)etechService.findListByHQL(hql,8);
-		session.setAttribute("supply", supply);
+		List<TcomInfo> supply = (List<TcomInfo>)etechService.findListByHQL(hql,10);
+		request.setAttribute("supply", supply);
 		
 		//下载
 		hql="From TdataCenter dataCenter where dataCenter.type='18'";
 		List<TdataCenter> downLoad = (List<TdataCenter>)etechService.findListByHQL(hql);
-		session.setAttribute("downLoad", downLoad);
+		request.setAttribute("downLoad", downLoad);
 		
 		// 每月需求排行榜:查询发布招聘最多的工种
 		hql="From Trecruit recruit from";
