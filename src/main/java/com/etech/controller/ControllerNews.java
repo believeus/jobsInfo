7e363290aa6e20b88401677680e87dcd7edf0f9a
wuhuanrong <569009496@qq.com>
2014-07-07 11:34:49 +0800

    前台分页

--- src/main/java/com/etech/controller/ControllerNews.java
+++ src/main/java/com/etech/controller/ControllerNews.java
@@ -3,14 +3,18 @@ package com.etech.controller;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.etech.entity.TdataCenter;
 import com.etech.service.EtechService;
+import com.etech.util.Page;
+import com.etech.util.Pageable;
 
 /**
  * 新闻详情
@@ -23,24 +27,29 @@ public class ControllerNews {
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/newsInfo", method = RequestMethod.GET)
-	public String newsInfoView(HttpSession session,Integer id) {
+	public String newsInfoView(HttpSession session,Integer id,HttpServletRequest request) {
 		TdataCenter dataCenter = (TdataCenter)etechService.findObjectById(TdataCenter.class, id);
-		session.setAttribute("data", dataCenter); 
+		request.setAttribute("data", dataCenter); 
 		
 		// 专题
 		String hql="From TdataCenter dataCenter where dataCenter.type='5'";
 		List<TdataCenter> subjectReport = (List<TdataCenter>)etechService.findListByHQL(hql);
-		session.setAttribute("subjectReport", subjectReport);
+		request.setAttribute("subjectReport", subjectReport);
 		
 		return "infoCenter/newsInfo";
 	}
 	
-	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/newsList", method = RequestMethod.GET)
-	public String newsListView(HttpSession session) {
-		String hql="From TdataCenter dataCenter where dataCenter.type='0'";
-		List<TdataCenter> news = (List<TdataCenter>)etechService.findListByHQL(hql, 10);
-		session.setAttribute("notices", news);
+	@RequestMapping(value = "/newsList")
+	public String newsListView(HttpSession session,HttpServletRequest request) {
+		String pageNumber = request.getParameter("pageNumber");
+		// 如果为空，则设置为1
+		if (StringUtils.isEmpty(pageNumber)) {
+			pageNumber="1";
+		}
+		String hql="From TdataCenter dataCenter where dataCenter.type='0' order by dataCenter.editTime desc";
+		Pageable pageable=new Pageable(Integer.valueOf(pageNumber),null);
+		Page<?> page = etechService.getPage(hql, pageable);
+		request.setAttribute("news",page);
 		return "infoCenter/newsList";
 	}
 }
--- src/main/webapp/WEB-INF/template/infoCenter/newsList.ftl
+++ src/main/webapp/WEB-INF/template/infoCenter/newsList.ftl
@@ -5,6 +5,10 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <meta http-equiv="imagetoolbar" content="no"/>
     <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <link href="/resource/public/js/admin/common.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="/resource/public/js/admin/jquery.js"></script>
+<script type="text/javascript" src="/resource/public/js/admin/list.js"></script>
+    
     <style type="text/css">
    		.j_main{
     		width:1000px;
@@ -121,31 +125,22 @@
 			<div class="j_main_right_1">
 				<p>
 					<img src="/resource/public/images/sanjiaojian.png" style="float: left; margin-right: 10px;">
-					所在位置 > 资讯中心
+					所在位置 > 资讯中心&nbsp;&nbsp;<span>共${news.total}条记录</span>
 				</p>
 			</div>
 			<div class="j_main_right_2" style="border:1px solid #e4e4e4;">
+			  <form id="listForm" action="newsList.jhtml">
 				<ul>
-					[#list news as new]
+					[#list news.content as new]
 					<li>
 						<a href="/newsInfo.jhtml?id=${new.id}">${new.title}</a><span>${new.editTime?number_to_datetime}</span>
 					</li>
 					[/#list]
 				</ul>
-				<p style="padding-left:25px;">
-					<a href="">首页</a>
-					<a href="">上一页</a>
-					<a href="">下一页</a>
-					<a href="">末页</a>
-					<select name="">
-						<option value="">1</option>
-						<option value="">2</option>
-						<option value="">3</option>
-						<option value="">4</option>
-						<option value="">5</option>
-					</select>
-					共<font color="red">5</font>页<font color="red">51</font>条
-				</p>
+				[@pagination pageNumber = news.pageNumber totalPages = news.totalPages]
+					[#include "/include/pagination.ftl"]
+				[/@pagination]
+				</form>
 			</div>
 		</div>
 	</div>
