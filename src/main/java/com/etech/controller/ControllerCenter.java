89c6d742903254285f328a120efcbf7ac7600b53
wuhuanrong <569009496@qq.com>
2014-07-11 19:04:47 +0800

    修改编辑器

--- src/main/java/com/etech/controller/ControllerCenter.java
+++ src/main/java/com/etech/controller/ControllerCenter.java
@@ -2,6 +2,8 @@ package com.etech.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -445,5 +447,39 @@ public class ControllerCenter {
 		Map<String, Object> map=new HashMap<String, Object>();
 		map.put("message", "success");
 		JsonOutToBrower.out(map, response);
+	}
+	@RequestMapping(value = "/UEpload", method = RequestMethod.POST, produces = "text/html; charset=UTF-8")
+	public  void UEpload(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		// 图片描述
+		String pictitle = request.getParameter("pictitle");
+		// 遍历有二进制文件的form表单
+		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+		String storepath = "";
+		Map<String, MultipartFile> files = multipartRequest.getFileMap();
+		for (MultipartFile file : files.values()) {
+			InputStream inputStream;
+			try {
+				inputStream = file.getInputStream();
+				String originName=file.getOriginalFilename();
+				String extention = originName.substring(originName.lastIndexOf(".") + 1);
+				log.debug("upload file stuffix:"+extention);
+				storepath = mydfsTrackerServer.upload(inputStream, extention);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("status", "SUCCESS");
+		map.put("title", "图片");
+		map.put("url", storepath);
+		String json="{\"url\":\""+storepath+"\", \"title\":\""+pictitle+"\", \"state\":\"SUCCESS\" }";
+		response.setContentType("text/html; charset=UTF-8");
+		OutputStream out = response.getOutputStream();
+		PrintWriter writer = new PrintWriter(out);
+		writer.println(json.toString());
+		writer.flush();
+		writer.close();
+		out.close();
 	}
 }
